0fd3cef1bcf2c97c0bf23681177cfa0d7fffc0a9
zgpeace <zgpeace@gmail.com>
2019-06-28 18:29:48 +0800

    Initial commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by zgpeace on 2019/6/28.
+//  Copyright © 2019 zgpeace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
